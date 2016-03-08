6c5f99564dfa433c902371504ef7fb46ecbe41d6
Federico Knüssel <fknussel@gmail.com>
2016-03-08 15:03:25 +1100

    Setup postcss

---
+++ grunt/postcss.js
@@ -0,0 +1,17 @@
+module.exports = {
+  options: {
+    processors: [
+      require('postcss-import')(),
+      require('postcss-simple-vars')(),
+      require('postcss-nested')(),
+      require('postcss-mixins')(),
+      require('pixrem')(),
+      require('autoprefixer')({ browsers: 'last 2 versions' }),
+      require('cssnano')()
+    ]
+  },
+  dist: {
+    src: 'src/css/app.css',
+    dest: 'dist/css/app.min.css'
+  }
+};
